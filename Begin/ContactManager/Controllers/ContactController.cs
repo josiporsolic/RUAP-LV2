6c7c54a0416b78e18e3f84eea6b87fcf6f3491ea
Josip Orsolic <josip.orsolic5@gmail.com>
2023-11-23 13:47:35 +0100

    Task 2 - Modifyng the Index View to Provide a GUI for Creating Contacts

--- Begin/ContactManager/Controllers/ContactController.cs
+++ Begin/ContactManager/Controllers/ContactController.cs
@@ -6,6 +6,9 @@ using System.Web.Mvc;
 using System.Web.Http;
 using ContactManager.Models;
 using ContactManager.Services;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
 
 namespace ContactManager.Controllers
 {
@@ -21,6 +24,14 @@ namespace ContactManager.Controllers
         public Contact[] Get()
         {
             return contactRepository.GetAllContacts();
+        }
+        public HttpResponseMessage Post(Contact contact)
+        {
+            this.contactRepository.SaveContact(contact);
+
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+
+            return response;
         }
     }
 }
